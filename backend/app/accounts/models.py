4c24a439ff77ee2971304f145b1fa01d7325f1c2
hector-medina <hector.medina.cabanelas@gmail.com>
2023-11-27 22:45:05 +0800

    add user model

--- backend/app/accounts/models.py
+++ backend/app/accounts/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+
+    phone = models.CharField(max_length=150, default=None, blank=True, null=True)
+    dni = models.CharField(max_length=150, default=None, blank=True, null=True)
+
     pass
