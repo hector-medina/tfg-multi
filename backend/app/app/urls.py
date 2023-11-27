8ff133abd9ffa5d59d215d0f781158910dfff3b9
hector-medina <hector.medina.cabanelas@gmail.com>
2023-11-27 22:45:25 +0800

    update urls

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from accounts.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r"accounts", UserViewSet, basename="accounts")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('', include(router.urls)),
 ]
