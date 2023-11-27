288835dd18ac705b125672ffe14dbb14bbda2b71
hector-medina <hector.medina.cabanelas@gmail.com>
2023-11-27 13:43:17 +0800

    fix typo

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_restframework',
-    'django_restframework.authtoken',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
