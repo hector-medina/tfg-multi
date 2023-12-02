8b8a5c93cf08cb277cbf64832fa31c1d4afda048
hector-medina <hector.medina.cabanelas@gmail.com>
2023-12-02 22:51:21 +0800

    añadir auth config

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
