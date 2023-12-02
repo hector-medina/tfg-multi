d0ab6acc82451edf6bcb433f6cbca316a306270a
hector-medina <hector.medina.cabanelas@gmail.com>
2023-12-02 22:51:27 +0800

    añadir urls login

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -17,12 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from accounts.views import UserViewSet
+from accounts.views import UserViewSet, UserRegistrationViewSet
+from rest_framework.authtoken import views
+
 
 router = routers.DefaultRouter()
 router.register(r"accounts", UserViewSet, basename="accounts")
+router.register("signup", UserRegistrationViewSet, basename="signup")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('login/', views.obtain_auth_token),
     path('', include(router.urls)),
 ]
