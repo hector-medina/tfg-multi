4fc87a27d9baa4eb0688c3ac990e5dfd2ca82796
hector-medina <hector.medina.cabanelas@gmail.com>
2023-12-10 16:20:26 +0800

    add routes

---
+++ frontend/app/Routes.js
@@ -0,0 +1,37 @@
+// IMPORTS
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// UI
+import { ThemeProvider } from "@rneui/themed"; 
+import theme from './src/theme';
+// Utils
+import CustomOptions from './src/utils/CustomOptions';
+// Imports of public Screens
+import LandingScreen from './src/screens/public/LandingScreen';
+import LoginScreen from './src/screens/public/LoginScreen';
+import ForgotPasswordScreen from './src/screens/public/ForgotPasswordScreen';
+import SignupScreen from './src/screens/public/SignupScreen';
+import TermsOfUseScreen from './src/screens/public/TermsofuseScreen'; 
+
+
+const PublicStack = createNativeStackNavigator();
+
+
+function PublicRoutes() {
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+          <PublicStack.Navigator initialRouteName="landing">
+              <PublicStack.Screen name="landing" component={LandingScreen} options={{headerShown: false}}/>
+              <PublicStack.Screen name="login" component={LoginScreen} options={{headerShown: false}}/>
+              <PublicStack.Screen name="forgotpassword" component={ForgotPasswordScreen} options={CustomOptions({title:'Forgot password'})}/>
+              <PublicStack.Screen name="signup" component={SignupScreen} options={{headerShown: false}}/>
+              <PublicStack.Screen name="termsofuse" component={TermsOfUseScreen} options={CustomOptions({title:'Terms of use'})}/>
+          </PublicStack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
+
+export default PublicRoutes
