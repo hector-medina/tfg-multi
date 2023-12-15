44e42d12c21417d6337580e5aee60ddd07bc681b
hector-medina <hector.medina.cabanelas@gmail.com>
2023-12-16 01:02:54 +0800

    test auth fetch private

--- frontend/app/src/screens/private/home/HomeScreen.jsx
+++ frontend/app/src/screens/private/home/HomeScreen.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Button } from "react-native";
+import { useSelector } from 'react-redux';
+
 import { fetchData } from '../../../api/utils/useFetch';
 
 const HomeScreen = () => {
 
-    
+    const authToken = useSelector((state) => state.auth.token);
+
     const getData = async () => {
         try{
             const url = '/accounts/';
-            const data = await fetchData({url: url});
-            console.log('esto es el data');
+            const data = await fetchData({url: url, authToken: authToken});
             console.log(data);
         }
         catch (error){
+            console.log(error);
         }
     }
    
