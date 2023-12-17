580fb4cd3fcfd122680e93c0d965aef5e2deebc2
hector-medina <hector.medina.cabanelas@gmail.com>
2023-12-17 19:46:06 +0800

    add private screens

--- frontend/app/src/screens/private/home/HomeScreen.jsx
+++ frontend/app/src/screens/private/home/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Button } from "react-native";
+import { StyleSheet, View, Text, ScrollView,TouchableWithoutFeedback } from "react-native";
 import { useSelector } from 'react-redux';
 
 import { fetchData } from '../../../api/utils/useFetch';
+import CardLarge from "../../../components/cards/CardLarge";
+import CardSmall from "../../../components/cards/CardSmall";
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
 
     const authToken = useSelector((state) => state.auth.token);
 
@@ -20,9 +22,53 @@ const HomeScreen = () => {
     }
    
     return (
-        <Button onPress={getData} title="GET DATA"/>
+        <ScrollView>
+             <View>
+                <Text style={ styles.title }>Finance</Text>
+                <CardLarge 
+                    url={'account'} 
+                    title='Account' 
+                    balance={'1064.87'} 
+                    income={'240.00'} 
+                    expenses={'-97.11'} 
+                    account_number={'2366'}
+                />
+
+                <CardLarge 
+                    url={'account'} 
+                    title='Debts' 
+                    balance={'260.00'} 
+                    income={'40.00'} 
+                    expenses={'-180.00'}
+                />
+            </View>
+            
+            <View>
+                <Text style={styles.title}>Reports</Text>
+                <View style={ styles.small_card_container }>
+                    <CardSmall url={'account'} title={'Agreements'}/>
+                    <CardSmall url={'account'} title={'Incidents'}/>
+                </View>
+                <View style={ styles.small_card_container }>
+                    <CardSmall url={'account'} title={'Invoices'}/>
+                    <CardSmall url={'account'} title={'Budgets'}/>
+                </View>
+            </View>
+            <View>
+                <Text style={ styles.title }>Announcements</Text>
+                <View style={ styles.small_card_container }>
+                    <CardSmall url={'account'} title={'Notifications'}/>
+                    <CardSmall url={'account'} title={'Polls'}/>
+                </View>
+            </View>
+        </ScrollView>
     );
 
 }
+
+const styles = StyleSheet.create({
+    title: {color: 'gray', fontSize: 25, fontWeight: 'bold', marginVertical: 10, marginHorizontal: 25},
+    small_card_container: {flexDirection: 'row', marginHorizontal: 20},
+});
 
 export default HomeScreen;
