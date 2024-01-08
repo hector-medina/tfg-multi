6ce869f9c6535ba68e6df219cb98e01da328a805
hector-medina <hector.medina.cabanelas@gmail.com>
2024-01-09 07:44:42 +0800

    add debt

---
+++ frontend/app/src/screens/private/home/debt/AddDebtScreen.jsx
@@ -0,0 +1,161 @@
+import React, { useEffect, useState } from "react";
+import { LogBox, Text, FlatList, StyleSheet, Button, ScrollView, Modal, View } from "react-native";
+import { useSelector } from 'react-redux';
+import { fetchData } from "../../../../api/utils/useFetch";
+import CustomButton from "../../../../components/buttons/CustomButton";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import theme from "../../../../theme";
+import CustomPicker from "../../../../components/forms/CustomPicker";
+import { useForm, Controller } from 'react-hook-form';
+import CardMedium from "../../../../components/cards/CardMedium";
+import ErrorModal from "../../../../components/modals/ErrorModal";
+import CardLargeMinimal from "../../../../components/cards/CardLargeMinimal";
+import CardSmall from "../../../../components/cards/CardSmall";
+import BasePrivateScreen from "../../BasePrivateScreen";
+import Loading from "../../../../components/loadings/Loading";
+import CustomInput from "../../../../components/forms/CustomInput";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import SuccessModal from "../../../../components/modals/SuccessModal";
+
+const AddDebtScreen = ({route, navigation}) => {
+    const {bankaccount_id} = route.params;
+    const authToken = useSelector((state) => state.auth.token);
+    const user_id =  useSelector((state) => state.user.id);
+
+    const { control, handleSubmit, errors, setValue, watch } = useForm();
+    const [modalVisible, setModalVisible] = useState(false);
+    const [sucessModalVisible, setSucessModalVisible] = useState(false);
+    const [ErrorModalVisible, setErrorModalVisible] = useState(true);
+    const [fetchErrors, setFetchErrors] = useState(null);
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const selectedDate = watch('transaction_date', '');
+  
+
+    const message_creation_successfully = [
+        "Your debt has been created successfully."
+    ]
+
+    const dissmiss = () => {
+        setFetchErrors(null);
+    }
+    
+    const createRecord = async (payload) => { 
+        payload.bank_account = bankaccount_id;
+        console.log(payload);
+        setModalVisible(true);
+        try {
+            var url = '/debts/';
+            var data = await fetchData({url: url, method:'POST', data:payload, authToken: authToken});
+            console.log(data)
+            setModalVisible(false);
+            setSucessModalVisible(true);
+        } catch(error) {
+            setModalVisible(false);
+            setErrorModalVisible(true)
+            setFetchErrors(error.message);
+        }
+    }
+
+    const handleDateChange = (event, date) => {
+        if (date !== undefined) {
+          setValue('transaction_date', date.toISOString().split('T')[0]);
+        }
+        setShowDatePicker(false);
+      };
+
+    return (
+        <BasePrivateScreen>
+        { sucessModalVisible && 
+            <SuccessModal 
+                message={message_creation_successfully} 
+                modalVisible={sucessModalVisible} 
+                onClose={() => {navigation.goBack()}}/>
+        }
+
+        { fetchErrors != null &&
+            <ErrorModal 
+                title={'Error'} 
+                message={fetchErrors} 
+                modalVisible={ErrorModalVisible} 
+                onClose={dissmiss}/>
+        }
+
+        <Loading 
+            title={'Creating'} 
+            message={'Your debt is processing.'} 
+            modalVisible={modalVisible}/>
+
+        <ScrollView>
+            <View style={ [theme.components.Card.style, styles.title] }>
+                    <Text style={ theme.components.Subtitle.style }>Here you can add a new debt:</Text>
+
+                    <CustomInput name='amount' 
+                                 placeholder='Amount' 
+                                 rules={ 
+                                    { 
+                                        required: "Amount of the debt is required."
+                                    } 
+                                 }
+                                 control={control}/>
+
+                    <CustomInput name='description' 
+                                 placeholder='Description'
+                                 rules={ 
+                                    { 
+                                        required: "Description of the debt is required."
+                                    } 
+                                 }
+                                 control={control}/>
+
+                    <Text style={ [theme.components.Subtitle.style, styles.field_title ] }>Date of the record: {selectedDate}</Text>
+
+                    <Controller
+                            rules = {{ required: "Date of the debt is required." }}
+                            control={control}
+                            render={({ field: { onChange, value }, fieldState: {error} }) => (
+                            <View>
+                                <View style={{flexDirection: 'row',justifyContent: 'space-between', alignItems: 'baseline'}}>
+                                    <CustomButton size='xs' onPressed={() => setShowDatePicker(true)}>
+                                        <Icon  name="calendar" size={15} color="#fff" />
+                                    </CustomButton>
+                                </View>
+                                <View>
+                                { error && 
+                                    (<Text style={styles.textError}>
+                                        {error.message || 'Error'}
+                                    </Text>)
+                                }
+                                </View>
+                                {showDatePicker && (
+                                <DateTimePicker
+                                    value={value ? new Date(value) : new Date()}
+                                    mode="date"
+                                    display="default"
+                                    onChange={handleDateChange}
+                                />
+                                )}
+                            </View>
+                            )}
+                            name="transaction_date"
+                            defaultValue=""
+                        />      
+
+                    <View style={styles.button}>
+                        <CustomButton size='xs'  onPressed={handleSubmit(createRecord)}>Add record</CustomButton>
+                    </View>
+            </View>
+        </ScrollView>
+    </BasePrivateScreen>
+    );
+};
+
+const styles = StyleSheet.create({
+    title: {marginTop: 20},
+    field_title: {marginTop: 20, alignSelf: 'flex-start'},
+    button: {marginTop: 20},
+    textError: {
+        color: 'red',
+    },
+});
+
+export default AddDebtScreen;
--- frontend/app/src/screens/private/home/debt/DebtScreen.jsx
+++ frontend/app/src/screens/private/home/debt/DebtScreen.jsx
@@ -1,15 +1,127 @@
-import React from "react";
-import { Text, StyleSheet, ScrollView, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { LogBox, Text, FlatList, StyleSheet, ScrollView, Modal, View } from "react-native";
+import { useSelector } from 'react-redux';
+import { fetchData } from "../../../../api/utils/useFetch";
+import CustomButton from "../../../../components/buttons/CustomButton";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import theme from "../../../../theme";
+import CustomPicker from "../../../../components/forms/CustomPicker";
+import { useForm } from 'react-hook-form';
+import CardMedium from "../../../../components/cards/CardMedium";
+import ErrorModal from "../../../../components/modals/ErrorModal";
+import CardLargeMinimal from "../../../../components/cards/CardLargeMinimal";
+import CardSmall from "../../../../components/cards/CardSmall";
 
-const DebtScreen = () => {
+const DebtScreen = ({route, navigation}) => {
+    LogBox.ignoreAllLogs()
+    const {bankaccount_id, community_id} = route.params;
+    const authToken = useSelector((state) => state.auth.token);
+    const user_id =  useSelector((state) => state.user.id);
+    const [bankAccount, setBankAccount] = useState({
+        id: 0, number: '', balance: 0.00, income: 0.00, expenses: 0.00, 
+        total_debt: 0.00, increased_debt: 0.00, decreased_debt: 0.00, debts: []});
+    const [community, setCommunity] = useState({
+        id: 0, name: '', admin: 0, president: 0});
+    const [debts, setDebts] = useState([]);
+
+    useEffect(() => {
+        if (user_id !== null && authToken !== null ) {
+            getData();
+            navigation.addListener('focus', () => {
+                getData();
+            })  
+        }
+    }, [user_id, authToken])
+
+    const getData = async () => {
+
+        try{
+            var url = '/bankaccounts/'+bankaccount_id+'/';
+            const data = await fetchData({url: url, authToken: authToken});
+            console.log(data);
+            setBankAccount(data);
+            
+            url = '/neighborhoods/'+community_id+'/';
+            const data_community = await fetchData({url: url, authToken: authToken});
+            console.log(data_community);
+            setCommunity(data_community);
+        }
+        catch (error){
+            console.log(error);
+        }
+    }
+
+    const Separador = () => (
+        <View
+          style={{
+            height: 1,
+            backgroundColor: '#ccc',
+            marginVertical: 5, 
+          }}
+        />
+      );
+
+      const itemList = ({item}) => {
+        return (
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.data}> {item.transaction_date}</Text>
+            </View>
+            <View style={{justifyContent: 'center'}}>
+                <Text style={[styles.amount_positive, item.amount >= 0 && styles.amount_negative]}> € {item.amount}</Text>
+            </View>
+         </View>
+        );
+      }
 
     return (
         <ScrollView>
-            <Text>Debt Screen</Text>
+            <CardLargeMinimal 
+                balance={bankAccount.total_debt.toFixed(2).toString()} 
+                income={bankAccount.decreased_debt.toFixed(2).toString()}
+                expenses={bankAccount.increased_debt.toFixed(2).toString()} 
+                />
+            <View style={{flexDirection: 'row',justifyContent: 'space-between', alignItems: 'center',marginHorizontal: 20, marginBottom: 20}}>
+                <Text style={{color: 'gray', fontWeight: 'bold', fontSize: 20}}>Debts</Text>
+                {(user_id == community.admin || user_id == community.president )?
+                 <CustomButton onPressed={() => {navigation.navigate('adddebt', {bankaccount_id: bankaccount_id})}} size='xs'>add</CustomButton>
+                 : null
+                }
+            </View>
+
+            <View style={[theme.components.Card.style, styles.card]}>            
+                <FlatList
+                    data={bankAccount.debts}
+                    renderItem={itemList}
+                    keyExtractor={item => item.id}
+                    ItemSeparatorComponent={Separador}
+                    />
+            </View>
         </ScrollView>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    card:{
+        marginHorizontal: 10,
+        borderRadius: 15, 
+    },
+    description: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    amount_positive: {
+        color: 'green',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    amount_negative: {
+        color: 'red',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+});
 
 export default DebtScreen;
