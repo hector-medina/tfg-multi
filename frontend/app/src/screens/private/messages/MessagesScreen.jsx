a3ee549c6b42cc0f87d1954e242e658b99d5e132
hector-medina <hector.medina.cabanelas@gmail.com>
2024-01-09 17:57:16 +0800

    update messages

--- frontend/app/src/screens/private/messages/MessagesScreen.jsx
+++ frontend/app/src/screens/private/messages/MessagesScreen.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { useSelector } from 'react-redux';
 
 const MessagesScreen = ({navigation}) => {
-   
+    const authToken = useSelector((state) => state.auth.token);
+    const user_id =  useSelector((state) => state.user.id);
+    const community_id = useSelector((state) => state.community.id);
     return (
-        <Text>Messages screen</Text>
+        <View>
+            <Text>Messages screen {user_id}</Text>
+            <Text>Messages screen {authToken}</Text>
+            <Text>Messages screen {community_id}</Text>
+        </View>
     );
 
 }
