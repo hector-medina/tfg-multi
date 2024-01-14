b031e76c300bb337b4178a4bf80403b6931de916
hector-medina <hector.medina.cabanelas@gmail.com>
2024-01-14 19:18:53 +0800

    update transform ignore patterns

--- frontend/app/jest.config.js
+++ frontend/app/jest.config.js
@@ -1,7 +1,7 @@
 const config = {
     preset: 'jest-expo',
     transformIgnorePatterns: [
-      'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?|@rneui)/)'
+      'node_modules/(?!((jest-)?react-native(-.*)?|@rneui/?|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-redux|@react-native-async-storage/async-storage)',
     ],
       transform: {
         '^.+\\.[jt]sx?$': 'babel-jest',
